3dbea348c07cd4d6e127690b7803ab4a5bc4fd68
KauanGabrie <kauangabrieldossantos012@gmail.com>
2024-05-29 14:35:24 -0300

    botao deslogar
    deu certo

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -4,40 +4,52 @@ import { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { pegarDadosPaciente } from '../servicos/PacienteServico'
 import { Paciente } from '../interfaces/Paciente'
+import { Botao } from '../componentes/Botao'
 
-export default function Perfil() {
+export default function Perfil({ navigation } : any){
   const [dadosPaciente, setDadosPaciente] = useState({} as Paciente)
 
   useEffect(() => {
     async function dadosPaciente() {
       const pacienteId = await AsyncStorage.getItem('pacienteId')
       if (!pacienteId) return null
-      
+
       const resultado = await pegarDadosPaciente(pacienteId)
-      if(resultado){
+      if (resultado) {
         setDadosPaciente(resultado)
         console.log(resultado)
       }
     }
     dadosPaciente()
   })
+  function deslogar() {
+    AsyncStorage.removeItem('token')
+    AsyncStorage.removeItem('pacienteId')
+    navigation.replace('Login')
+  }
   return (
     <ScrollView flex={1}>
       <VStack flex={1} alignItems="center" p={5}>
         <Titulo color="blue.500">Meu Perfil</Titulo>
 
-        <Avatar size="xl" source={{ uri: "https://github.com/robertoSRMJunior.png" }} mt={5} />
+        <Avatar size="xl" source={{ uri: dadosPaciente.imagem }} mt={5} />
 
         <Titulo color="blue.500">Informações pessoais</Titulo>
         <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
-        <Text>01/04/1988</Text>
-        <Text>São Paulo</Text>
+        <Text>{dadosPaciente.email}</Text>
+        <Text>dadosPaciente.endereco.estado</Text>
 
         <Divider mt={5} />
 
-        <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
-        <Text>Bronquite</Text>
-        <Text>Sinusite</Text>
+        <Titulo color="blue.500" mb={1}>Planos de saúde</Titulo>
+        {
+          dadosPaciente.planosSaude?.map((plano, index) => (
+            <Text key={index}>{plano}</Text>
+          ))
+        }
+        <Botao onPress={() => deslogar()} >
+          Deslogar
+        </Botao>
       </VStack>
     </ScrollView>
   )
